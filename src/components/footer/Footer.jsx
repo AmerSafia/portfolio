87ecb9d02b6d4d20bcf4f0941ae9a21a0e22f9f8
MohammadSafia <amer.safia321@gmail.com>
2022-07-27 18:15:49 +0300

    edit nav and add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,87 @@
-import './footer.css'
-import React from 'react'
+import "./footer.css";
+import React from "react";
+import { FaFacebookF } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { IoLogoTwitter } from "react-icons/io";
+import { AiOutlineLinkedin, AiOutlineGithub } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#Home" rel="noopener noreferrer" className="footer__logo">
+        Amer Safia
+      </a>
+      <ul className="preimalink">
+        <li>
+          <a href="#Home" rel="noopener noreferrer">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#about" rel="noopener noreferrer">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#experience" rel="noopener noreferrer">
+            Experience
+          </a>
+        </li>
+        <li>
+          <a href="#protofile" rel="noopener noreferrer">
+            Protofile
+          </a>
+        </li>
+        <li>
+          <a href="#contact" rel="noopener noreferrer">
+            Contact
+          </a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="footer__soicals">
+        <a
+          href="https://web.facebook.com/amer.safia/"
+          target={"_blank"}
+          rel="noopener noreferrer"
+        >
+          <FaFacebookF />
+        </a>
+        <a
+          href="https://www.instagram.com/amer.safia/"
+          target={"_blank"}
+          rel="noopener noreferrer"
+        >
+          <FiInstagram />
+        </a>
+        <a
+          href="https://twitter.com/Amersafia1"
+          target={"_blank"}
+          rel="noopener noreferrer"
+        >
+          <IoLogoTwitter />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/amer-safia-b463a115a/"
+          target={"_blank"}
+          rel="noopener noreferrer"
+        >
+          <AiOutlineLinkedin />
+        </a>
+        <a
+          href="https://github.com/AmerSafia"
+          target={"_blank"}
+          rel="noopener noreferrer"
+        >
+          <AiOutlineGithub />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small> &copy;2022 Copyright. All right reserved. </small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,8 @@
 import './nav.css'
 import React from 'react'
 import {AiOutlineHome,AiOutlineUser} from 'react-icons/ai'
-import {BiBook,BiMessageSquareDetail} from 'react-icons/bi'
-import {RiServiceLine} from 'react-icons/ri'
+import {BiBook,BiMessageSquareDetail,} from 'react-icons/bi'
+import {BsFillPersonFill} from 'react-icons/bs'
 import { useState } from 'react'
 const Nav = () => {
 const [activeNav,setActveNav]=  useState('#Home')
@@ -11,7 +11,7 @@ const [activeNav,setActveNav]=  useState('#Home')
       <a href="#Home" onClick={()=>setActveNav('#Home')} className={activeNav==='#'?'active':''}><AiOutlineHome/></a>
       <a href="#about" onClick={()=>setActveNav('#about')} className={activeNav==='#about'?'active':''} ><AiOutlineUser/></a>
       <a href="#experience" onClick={()=>setActveNav('#experience')} className={activeNav==='#experience'?'active':''}><BiBook/></a>
-      <a href="#services" onClick={()=>setActveNav('#services')} className={activeNav==='#services'?'active':''}><RiServiceLine/></a>
+      <a href="#protofile" onClick={()=>setActveNav('#protofile')} className={activeNav==='#protofile'?'active':''}><BsFillPersonFill/></a>
       <a href="#contact" onClick={()=>setActveNav('#contact')} className={activeNav==='#contact'?'active':''}><BiMessageSquareDetail/></a>
     </nav>
   )
